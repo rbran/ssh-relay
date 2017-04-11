ac50094dea7dd42f7f1e38b69cc5e7d7813bb04a
rubens-brandao <rubens.brandao@ubnt.com>
2017-04-11 15:45:22 -0300

    Now server uses threads and improved read/write loop

--- server.py
+++ server.py
@@ -7,6 +7,7 @@ import base64
 from binascii import hexlify
 import os
 import socket
+import select
 import sys
 import threading
 import traceback
@@ -84,10 +85,12 @@ class Server (paramiko.ServerInterface):
         return True
 
     def check_channel_shell_request(self, channel):
+        ok('Client request shell')
         self.event.set()
         return True
 
     def check_channel_pty_request(self, channel, term, width, height, pixelwidth, pixelheight, modes):
+        ok('Client request pty')
         self.term = term
         self.width = width
         self.height = height
@@ -156,152 +159,158 @@ if __name__ == '__main__':
         traceback.print_exc()
         sys.exit(1)
 
+
+    class TunnelThread(threading.Thread):
+
+        def __init__(self, ip, port, socket):
+            threading.Thread.__init__(self)
+            self.ip = ip
+            self.port = port
+            self.socket = socket
+
+
+        def run(self):
+            info('Got a connection from %s:%d' % (self.ip, self.port))
+
+            client_log_f = ''
+            server_log_f = ''
+            path = ''
+
+            try:
+                t = paramiko.Transport(self.socket, gss_kex=False)
+                if args.gex:
+                    try:
+                        t.load_server_moduli()
+                    except:
+                        error('(Failed to load moduli -- gex will be unsupported.)')
+                        raise
+                t.add_server_key(host_key)
+                server = Server()
+                try:
+                    t.start_server(server=server)
+                except paramiko.SSHException:
+                    error('*** SSH negotiation failed.')
+                    t.close()
+                    return
+
+                # wait for auth
+                client_chan = t.accept(20)
+                if client_chan is None:
+                    error('*** No channel.')
+                    t.close()
+                    return
+                ok('Received Authentication!')
+
+                if args.alerting:
+                    client_chan.send(
+                        '\r\n\033[31;1mThis connection has been intercepted\033[0m\r\n')
+
+                start_time = datetime.datetime.now()
+                last_time = start_time
+                delta = ""
+
+                server_ssh = paramiko.SSHClient()
+                server_ssh.load_system_host_keys()
+                server_ssh.set_missing_host_key_policy(paramiko.WarningPolicy())
+                try:
+                    server_ssh.set_missing_host_key_policy(paramiko.client.AutoAddPolicy())
+                    server_ssh.connect(
+                        args.remote_server, args.remote_port, server.username, server.password)
+                except paramiko.AuthenticationException as e:
+                    client_chan.send("authentification failed\r\n")
+                    client_chan.close()
+                    return
+
+                if args.asciinema:
+                    now = datetime.datetime.now()
+                    path = "%s/%04d-%02d-%02dH%02d:%02d:%02d.%06d_%s-%d" % (
+                        args.asciinema, now.year, now.month, now.day, now.hour, now.minute, now.second, now.microsecond, self.ip, self.port)
+
+                    ok("asciinema session saved : %s" % (path))
+                    if not os.path.exists(path):
+                        os.makedirs(path)
+
+                    asciinema_data_hdr = '{\n'
+                    asciinema_data_hdr += '  "version": 1,\n'
+
+                    if server.command == b'':
+                        asciinema_data_hdr += '  "command": "$SHELL",\n'
+                        asciinema_data_hdr += '  "width": %d,\n' % (server.width)
+                        asciinema_data_hdr += '  "height": %d,\n' % (server.height)
+                    else:
+                        asciinema_data_hdr += '  "command": %s,\n' % (json.dumps(server.command.decode("utf-8")))
+
+                    asciinema_data_hdr += '}\n'
+
+                    filename = path + "/data.json"
+                    f = open(filename, "w")
+                    f.write(asciinema_data_hdr)
+                    f.close()
+                    client_log_f = open(path + "/client.raw", "wb")
+                    server_log_f = open(path + "/server.raw", "wb")
+
+                server_chan = ''
+                if server.command != b'':
+                    server_chan = server_ssh.get_transport().open_session()
+                    server_chan.exec_command(server.command)
+                else:
+                    server_chan = server_ssh.invoke_shell()
+
+                #Max 1 min Iddle
+                server_chan.settimeout(60.0)
+                client_chan.settimeout(60.0)
+
+                while True:
+                    r, w, e = select.select([server_chan, client_chan], [], [])
+                    if client_chan in r:
+                        x = client_chan.recv(1024)
+                        client_log_f.write(x)
+                        x = u(x)
+                        if len(x) == 0:
+                            break
+                        server_chan.send(x)
+                    if server_chan in r:
+                        x = server_chan.recv(1024)
+                        server_log_f.write(x)
+                        x = u(x)
+                        if len(x) == 0:
+                            break
+                        client_chan.send(x)
+
+                server_chan.close()
+                client_chan.close()
+                t.close()
+                if args.asciinema:
+                    client_log_f.close()
+                    server_log_f.close()
+                ok('Connection Closed!')
+            except Exception as e:
+                error('*** Caught exception: ' + str(e.__class__) + ': ' + str(e))
+                traceback.print_exc()
+                try:
+                    t.close()
+                except:
+                    pass
+
     try:
-        sock.listen(100)
+        sock.listen(10)
     except Exception as e:
         error('*** Listen failed: ' + str(e))
         traceback.print_exc()
         sys.exit(1)
 
+    threads = []
+    info('Listening for new connection ...')
     while True:
         try:
-            info('Listening for new connection ...')
             client, addr = sock.accept()
+            newthread = TunnelThread(addr[0], int(addr[1]), client)
+            newthread.start()
+            threads.append(newthread)
         except Exception as e:
             error('*** accept failed: ' + str(e))
             traceback.print_exc()
             sys.exit(1)
 
-        client_ip = addr[0]
-        client_port = int(addr[1])
-        info('Got a connection from %s:%d' % (client_ip, client_port))
-
-        input_f = ''
-        output_f = ''
-        path = ''
-
-        try:
-            t = paramiko.Transport(client, gss_kex=False)
-            if args.gex:
-                try:
-                    t.load_server_moduli()
-                except:
-                    error('(Failed to load moduli -- gex will be unsupported.)')
-                    raise
-            t.add_server_key(host_key)
-            server = Server()
-            try:
-                t.start_server(server=server)
-            except paramiko.SSHException:
-                error('*** SSH negotiation failed.')
-                t.close()
-                continue
-
-            # wait for auth
-            chan = t.accept(20)
-            if chan is None:
-                error('*** No channel.')
-                t.close()
-                continue
-            ok('Received Authentication!')
-
-            if args.alerting:
-                chan.send(
-                    '\r\n\033[31;1mThis connection has been intercepted\033[0m\r\n')
-
-            start_time = datetime.datetime.now()
-            last_time = start_time
-            delta = ""
-
-            remote = paramiko.SSHClient()
-            remote.load_system_host_keys()
-            remote.set_missing_host_key_policy(paramiko.WarningPolicy())
-            try:
-                remote.set_missing_host_key_policy(paramiko.client.AutoAddPolicy())
-                remote.connect(
-                    args.remote_server, args.remote_port, server.username, server.password)
-            except paramiko.AuthenticationException as e:
-                chan.send("authentification failed\r\n")
-                chan.close()
-                continue
-
-            if args.asciinema:
-                now = datetime.datetime.now()
-                path = "%s/%04d-%02d-%02d-%02dH%02d:%02d.%06d_%s-%d" % (
-                    args.asciinema, now.year, now.month, now.day, now.hour, now.minute, now.second, now.microsecond, client_ip, client_port)
-
-                if not os.path.exists(path):
-                    os.makedirs(path)
-
-                asciinema_data_hdr = '{\n'
-                asciinema_data_hdr += '  "version": 1,\n'
-
-                if server.command == b'':
-                    asciinema_data_hdr += '  "command": "$SHELL",\n'
-                    asciinema_data_hdr += '  "width": %d,\n' % (server.width)
-                    asciinema_data_hdr += '  "height": %d,\n' % (server.height)
-                else:
-                    asciinema_data_hdr += '  "command": %s,\n' % (json.dumps(server.command.decode("utf-8")))
-
-                asciinema_data_hdr += '}\n'
-
-                filename = path + "/data.json"
-                f = open(filename, "w")
-                f.write(asciinema_data_hdr)
-                f.close()
-                input_f = open(path + "/input.raw", "wb")
-                output_f = open(path + "/output.raw", "wb")
-
-            remote_chan = ''
-            if server.command != b'':
-                remote_chan = remote.get_transport().open_session()
-                remote_chan.exec_command(server.command)
-            else:
-                remote_chan = remote.invoke_shell()
-
-            while True:
-                time.sleep(0.01)
-                server_data = b''
-                if remote_chan.closed or chan.closed:
-                    break
-                while True:
-                    if remote_chan.closed or chan.closed:
-                        break
-                    if remote_chan.recv_ready():
-                        tmp = remote_chan.recv(1024)
-                        server_data += tmp
-                        output_f.write(tmp)
-                    else:
-                        if server_data == b'':
-                            break
-                        chan.send(server_data)
-                        server_data = b''
-                        time.sleep(0.01)
-                        break
-                client_data = b''
-                while True:
-                    if remote_chan.closed or chan.closed:
-                        break
-                    if chan.recv_ready():
-                        tmp = chan.recv(1024)
-                        input_f.write(tmp)
-                        client_data += tmp
-                    else:
-                        if client_data == b'':
-                            break
-                        remote_chan.send(client_data)
-                        client_data = b''
-                        time.sleep(0.01)
-                        break
-            remote_chan.close()
-            chan.close()            
-            if args.asciinema:
-                input_f.close()
-                output_f.close()
-        except Exception as e:
-            error('*** Caught exception: ' + str(e.__class__) + ': ' + str(e))
-            traceback.print_exc()
-            try:
-                t.close()
-            except:
-                pass
+    for t in threads:
+        t.join()
